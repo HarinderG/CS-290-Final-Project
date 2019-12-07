00c6b8df66dd2e2a85ea5aa4c5942efacb98dbe8
David Shoemaker <shoemakerdavid@yahoo.com>
2019-12-07 15:33:35 -0800

    hide and show modal should work. Not search yet.

--- public/index.js
+++ public/index.js
@@ -51,6 +51,92 @@ function search() {
 search(title);
 
 
+//----------------------------------------------------------------------------------
+
+
+var advancedSearchButton = document.getElementById('Advanced-Search-Modal');
+if(advancedSearchButton){
+	advancedSearchButton.addEventListener('click', showAdvancedSearchModal);
+}
+
+//make function
+var modalAcceptButton = document.getElementById('modal-accept');
+  if (modalAcceptButton) {
+    modalAcceptButton.addEventListener('click', HandleAdvancedSearchAcceptClick);
+  }
+  
+var modalCancelButton = document.getElementById('modal-cancel');
+	if('modal-cancel'){
+		modalCancelButton.addEventListener('click', hideAdvancedSearchModal);
+	}
+
+function showAdvancedSearchModal(){
+
+	var showAdvancedSearchModal = document.getElementById('Advanced-Search-Modal');
+	var modalBackDrop = document.getElementById('modal-backdrop');
+
+	showAdvancedSearchModal.ClassList.remove('hidden');
+	modalBackDrop.ClassList.remove('hidden');
+}
+
+function hideAdvancedSearchModal(){
+	var showAdvancedSearchModal = document.getElementById('Advanced-Search-Modal');
+	var modalBackDrop = document.getElementById('modal-backdrop');
+
+	showAdvancedSearchModal.ClassList.add('hidden');
+	modalBackDrop.ClassList.add('hidden');
+
+}
+
+function HandleAdvancedSearchAcceptClick(){
+
+	doFilterUpdate();
+
+	hideAdvancedSearchModal();
+
+}
+
+
+function doFilterUpdate() {
+
+  /*
+   * Grab values of filters from user inputs.
+   */
+  var filters = {
+    movieTitle: document.getElementById('filter-text').value.trim(),
+    minScore: document.getElementById('filter-min-price').value,
+    maxScore: document.getElementById('filter-max-price').value,
+    year: document.getElementById('filter-year').value,
+    Actor: document.getElementById('filter-actor').value
+
+  }
+
+
+
+  /*
+   * Remove all "post" elements from the DOM.
+   */
+  var postContainer = document.getElementById('posts');
+  while(postContainer.lastChild) {
+    postContainer.removeChild(postContainer.lastChild);
+  }
+
+  /*
+   * Loop through the collection of all "post" elements and re-insert ones
+   * that meet the current filtering criteria.
+   */
+  allPosts.forEach(function (post) {
+    if (postPassesFilters(post, filters)) {
+      insertNewPost(post.description, post.photoURL, post.price, post.city, post.condition);
+    }
+  });
+
+}
+
+
+
+
+
 
 
 

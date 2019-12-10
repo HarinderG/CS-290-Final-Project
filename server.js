2562d2354694117392d921010f21c2cb73869cc9
HarinderG <harinder.gakhal@gmail.com>
2019-12-09 17:26:14 -0800

    Add server routing for search term and page number

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
-/*var postData = require("./postData.json");
-var test;*/
-
 var fetch = require("node-fetch");
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-app.get("*", async function (req, res) {
+app.get("/", async function (req, res) {
 	await fetch('http://www.omdbapi.com/?s=' + "cars" +'&apikey=e583146d')
 		.then(response => {
 			// Get JSON
@@ -22,7 +19,28 @@ app.get("*", async function (req, res) {
 		})
 		.then(data => {
 			// Work with JSON data here
-			res.status(200).render('moviePage', {data});
+			var apiData = data.Search;
+			res.status(200).render('moviePage', {apiData});
+		})
+		.catch(err => {
+			// Do something for an error here
+			console.log("------------------ URL DOES NOT EXIST");
+		})
+});
+
+app.get("/:s/:n", async function (req, res) {
+	var search = req.params.s;
+	var pageNum = req.params.n;
+
+	await fetch('http://www.omdbapi.com/?s=' + search + "&page=" + pageNum +'&apikey=e583146d')
+		.then(response => {
+			// Get JSON
+			return response.json()
+		})
+		.then(data => {
+			// Work with JSON data here
+			var apiData = data.Search;
+			res.status(200).render('moviePage', {apiData});
 		})
 		.catch(err => {
 			// Do something for an error here

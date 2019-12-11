fa1c75164eab358d869076c5390ef2c372dafdc1
HarinderG <harinder.gakhal@gmail.com>
2019-12-10 19:23:04 -0800

    Display all possible results

--- server.js
+++ server.js
@@ -20,25 +20,31 @@ async function displayMovies (req, res) {
 	var search = req.params.s;
 	var titleList = [];
 	var apiData = [];
+	var apiRespone = "True";
+	var pageN = 1;
 
 	if (search == undefined)
 		search = "star wars";
 
-	for (var pageN = 1; pageN <= 6; pageN++) {
+	while (apiRespone.indexOf("True") != -1) {
 		await fetch('http://www.omdbapi.com/?s=' + search + "&page=" + pageN +'&apikey=' + config.API_KEY)
 			.then(response => {
 				return response.json();
 			})
 			.then(data => {
+				apiRespone = data.Response;
+				if (apiRespone.indexOf("True") != -1) {
 					for (var i = 0; i < data.Search.length; i++) {
 						console.log("Found " + data.Search[i].Title + " on page " + pageN);
-						if (data.Search[i].Type.indexOf("movie") != -1 && titleList.indexOf(data.Search[i].Title) == -1)
+						if (data.Search[i].Type.indexOf("movie") != -1 && titleList.indexOf(data.Search[i].Title) == -1 && data.Search[i].Poster.indexOf("N/A") == -1)
 							titleList.push(data.Search[i].Title);
 					}
+				}
 			})
 			.catch(err => {
 				console.log("------------------ URL DOES NOT EXIST");
 			});
+			pageN++;
 		}
 
 		for (var i = 0; i < titleList.length; i++){

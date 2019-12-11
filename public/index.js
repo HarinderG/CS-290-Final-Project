a73ec99f4262bbb32292eb694b767b16c821f58f
David Shoemaker <shoemakerdavid@yahoo.com>
2019-12-10 19:08:09 -0800

    added filter by genre functionality

--- public/index.js
+++ public/index.js
@@ -42,6 +42,178 @@ wantedDecade2000to2009.addEventListener('click', delDecade2000to2009);
 var wantedDecade2010to2019 = document.getElementById('dropdown-decade-2010');
 wantedDecade2010to2019.addEventListener('click', delDecade2010to2019);
 
+var wantedGenreAction = document.getElementById('dropdown-genre-action');
+wantedGenreAction.addEventListener('click', getGenreAction);
+var wantedGenreAdventure = document.getElementById('dropdown-genre-adventure');
+wantedGenreAdventure.addEventListener('click', getGenreAdventure);
+var wantedGenreComedy = document.getElementById('dropdown-genre-comedy');
+wantedGenreComedy.addEventListener('click', getGenreComedy);
+var wantedGenreCrime = document.getElementById('dropdown-genre-crime');
+wantedGenreCrime.addEventListener('click', getGenreCrime);
+var wantedGenreDrama = document.getElementById('dropdown-genre-drama');
+wantedGenreDrama.addEventListener('click', getGenreDrama);
+var wantedGenreFamily = document.getElementById('dropdown-genre-family');
+wantedGenreFamily.addEventListener('click', getGenreFamily);
+var wantedGenreHistory = document.getElementById('dropdown-genre-history');
+wantedGenreHistory.addEventListener('click', getGenreHistory);
+var wantedGenreHorror = document.getElementById('dropdown-genre-horror');
+wantedGenreHorror.addEventListener('click', getGenreHorror);
+var wantedGenreMystery = document.getElementById('dropdown-genre-mystery');
+wantedGenreMystery.addEventListener('click', getGenreMystery);
+var wantedGenreRomance = document.getElementById('dropdown-genre-romance');
+wantedGenreRomance.addEventListener('click', getGenreRomance);
+
+var wantedGenreThriller = document.getElementById('dropdown-genre-thriller');
+wantedGenreThriller.addEventListener('click', getGenreThriller);
+
+
+
+function getGenreThriller(){
+	var specifiedGenre = 'Thriller';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+
+function getGenreRomance(){
+	var specifiedGenre = 'Romance';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreMystery(){
+	var specifiedGenre = 'Mystery';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreHorror(){
+	var specifiedGenre = 'Horror';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreHistory(){
+	var specifiedGenre = 'History';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreFamily(){
+	var specifiedGenre = 'Family';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreDrama(){
+	var specifiedGenre = 'Drama';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreCrime(){
+	var specifiedGenre = 'Crime';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreComedy(){
+	var specifiedGenre = 'Comedy';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreAdventure(){
+	var specifiedGenre = 'Adventure';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+}
+
+function getGenreAction(){
+
+	var specifiedGenre = 'Action';
+	var allCards = document.querySelectorAll(".card");
+
+	for(i = 0; i < allCards.length; i++){
+		var genreContentOfAllCardsArray = allCards[i].getAttribute("card-genre");
+		console.log(genreContentOfAllCardsArray)
+	 	if(genreContentOfAllCardsArray.indexOf(specifiedGenre) == -1){
+			allCards[i].parentNode.removeChild(allCards[i]);
+		}
+	}
+
+}
+
 
 function delDecade60to69(){
 	var specifiedYear = '196';

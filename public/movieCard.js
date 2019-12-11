05a3a20058907e315065d7dcda93b11b5fec5ee1
Harinder Gakhal <harinder.gakhal@gmail.com>
2019-12-11 00:49:01 -0800

    Make images clickable

--- public/movieCard.js
+++ public/movieCard.js
@@ -13,7 +13,9 @@ templates['movieCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(con
     + alias4(((helper = (helper = helpers.Poster || (depth0 != null ? depth0.Poster : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Poster","hash":{},"data":data,"loc":{"start":{"line":4,"column":16},"end":{"line":4,"column":26}}}) : helper)))
     + "\" alt=\""
     + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":4,"column":33},"end":{"line":4,"column":42}}}) : helper)))
-    + "\">\r\n    </div>\r\n    <div class=\"card-info-container\">\r\n      <span class=\"card-title\">"
+    + "\" href=\"http://localhost:3000/"
+    + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":4,"column":72},"end":{"line":4,"column":81}}}) : helper)))
+    + "/info\">\r\n    </div>\r\n    <div class=\"card-info-container\">\r\n      <span class=\"card-title\">"
     + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":7,"column":31},"end":{"line":7,"column":40}}}) : helper)))
     + "</span>\r\n      <span class=\"card-year\">"
     + alias4(((helper = (helper = helpers.Year || (depth0 != null ? depth0.Year : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Year","hash":{},"data":data,"loc":{"start":{"line":8,"column":30},"end":{"line":8,"column":38}}}) : helper)))

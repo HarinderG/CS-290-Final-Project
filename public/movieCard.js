841310d24d7921dcea41b7f198f3b494abd15b65
HarinderG <harinder.gakhal@gmail.com>
2019-12-11 18:24:54 -0800

    Add caching system to server.js

--- public/movieCard.js
+++ public/movieCard.js
@@ -9,16 +9,16 @@ templates['movieCard'] = template({"compiler":[8,">= 4.3.0"],"main":function(con
     + alias4(((helper = (helper = helpers.Rated || (depth0 != null ? depth0.Rated : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Rated","hash":{},"data":data,"loc":{"start":{"line":1,"column":53},"end":{"line":1,"column":62}}}) : helper)))
     + "\" card-metascore=\""
     + alias4(((helper = (helper = helpers.Metascore || (depth0 != null ? depth0.Metascore : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Metascore","hash":{},"data":data,"loc":{"start":{"line":1,"column":80},"end":{"line":1,"column":93}}}) : helper)))
-    + "\">\r\n  <div class=\"card-contents\">\r\n    <div class=\"card-image-container\">\r\n      <img src=\""
-    + alias4(((helper = (helper = helpers.Poster || (depth0 != null ? depth0.Poster : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Poster","hash":{},"data":data,"loc":{"start":{"line":4,"column":16},"end":{"line":4,"column":26}}}) : helper)))
-    + "\" alt=\""
+    + "\">\r\n  <div class=\"card-contents\">\r\n    <div class=\"card-image-container\">\r\n		<a href=\"http://localhost:3000/"
     + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":4,"column":33},"end":{"line":4,"column":42}}}) : helper)))
-    + "\" href=\"http://localhost:3000/"
-    + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":4,"column":72},"end":{"line":4,"column":81}}}) : helper)))
-    + "/info\">\r\n    </div>\r\n    <div class=\"card-info-container\">\r\n      <span class=\"card-title\">"
-    + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":7,"column":31},"end":{"line":7,"column":40}}}) : helper)))
+    + "/info\">\r\n	      <img src=\""
+    + alias4(((helper = (helper = helpers.Poster || (depth0 != null ? depth0.Poster : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Poster","hash":{},"data":data,"loc":{"start":{"line":5,"column":17},"end":{"line":5,"column":27}}}) : helper)))
+    + "\" alt=\""
+    + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":5,"column":34},"end":{"line":5,"column":43}}}) : helper)))
+    + "\" >\r\n		</a>\r\n    </div>\r\n    <div class=\"card-info-container\">\r\n      <span class=\"card-title\">"
+    + alias4(((helper = (helper = helpers.Title || (depth0 != null ? depth0.Title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Title","hash":{},"data":data,"loc":{"start":{"line":9,"column":31},"end":{"line":9,"column":40}}}) : helper)))
     + "</span>\r\n      <span class=\"card-year\">"
-    + alias4(((helper = (helper = helpers.Year || (depth0 != null ? depth0.Year : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Year","hash":{},"data":data,"loc":{"start":{"line":8,"column":30},"end":{"line":8,"column":38}}}) : helper)))
+    + alias4(((helper = (helper = helpers.Year || (depth0 != null ? depth0.Year : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"Year","hash":{},"data":data,"loc":{"start":{"line":10,"column":30},"end":{"line":10,"column":38}}}) : helper)))
     + "</span>\r\n    </div>\r\n  </div>\r\n</div>\r\n";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var exphbs = require('express-handlebars');
 var config = require('./config.js');
 var app = express();
 var port = process.env.PORT || 3000;
-
 var fetch = require("node-fetch");
+var fs = require("fs");
 
 app.engine('handlebars', exphbs({}));
 app.set('view engine', 'handlebars');
@@ -44,46 +44,67 @@ async function displayMovies (req, res) {
 	if (search == undefined)
 		search = "star wars";
 
-	for (var pageN; pageN <= 10; pageN++) {
-		await fetch('http://www.omdbapi.com/?s=' + search + "&page=" + pageN +'&apikey=' + config.API_KEY)
-			.then(response => {
-				return response.json();
-			})
-			.then(data => {
-				apiRespone = data.Response;
-				if (apiRespone.indexOf("True") != -1) {
-					for (var i = 0; i < data.Search.length; i++) {
-						console.log("Found " + data.Search[i].Title + " on page " + pageN);
-						if (data.Search[i].Type.indexOf("movie") != -1 && titleList.indexOf(data.Search[i].Title) == -1 && data.Search[i].Poster.indexOf("N/A") == -1)
-							titleList.push(data.Search[i].Title);
-					}
-				}
-			})
-			.catch(err => {
-				console.log("------------------ URL DOES NOT EXIST");
-			});
-		}
-
-		for (var i = 0; i < titleList.length; i++){
-			await fetch('http://www.omdbapi.com/?t=' + titleList[i] +'&apikey=' + config.API_KEY)
+	if (fs.existsSync('./cachedData/' + search + '.json')) {
+		fs.readFile('./cachedData/' + search + '.json', 'utf-8', (err, data) => {
+			if (err) throw err;
+			console.log("FOUND " + search + " IN CACHE");
+			var apiData2 = JSON.parse(data);
+			res.status(200).render('moviePage', {apiData2});
+		});
+	}
+	else {
+		console.log(search + " DNE");
+		for (var pageN; pageN <= 12; pageN++) {
+			await fetch('http://www.omdbapi.com/?s=' + search + "&page=" + pageN +'&apikey=' + config.API_KEY)
 				.then(response => {
 					return response.json();
 				})
 				.then(data => {
-					if (data.Title.indexOf("undefined") == -1) {
-						console.log("Adding == : " + data.Title);
-						if (data.Poster.indexOf("N/A") == -1 && data.Type.indexOf("movie") != -1)
-							apiData.push(data);
+					apiRespone = data.Response;
+					if (apiRespone.indexOf("True") != -1) {
+						for (var i = 0; i < data.Search.length; i++) {
+							console.log("Found " + data.Search[i].Title + " on page " + pageN);
+							if (data.Search[i].Type.indexOf("movie") != -1 && titleList.indexOf(data.Search[i].Title) == -1 && data.Search[i].Poster.indexOf("N/A") == -1)
+								titleList.push(data.Search[i].Title);
+						}
 					}
 				})
 				.catch(err => {
 					console.log("------------------ URL DOES NOT EXIST");
 				});
-		}
-		if (apiData.length == 0)
-			res.status(200).render('badSearch');
-		else
-			res.status(200).render('moviePage', {apiData});
+			}
+
+			for (var i = 0; i < titleList.length; i++){
+				await fetch('http://www.omdbapi.com/?t=' + titleList[i] +'&apikey=' + config.API_KEY)
+					.then(response => {
+						return response.json();
+					})
+					.then(data => {
+						if (data.Title.indexOf("undefined") == -1) {
+							console.log("Adding == : " + data.Title);
+							if (data.Poster.indexOf("N/A") == -1 && data.Type.indexOf("movie") != -1)
+								apiData.push(data);	
+						}
+					})
+					.catch(err => {
+						console.log("------------------ URL DOES NOT EXIST");
+					});
+			}
+			if (apiData.length == 0)
+				res.status(200).render('badSearch');
+			else {
+				fs.writeFile('./cachedData/' + search + '.json', JSON.stringify(apiData), 'utf-8', (err) => {
+				    if (err) throw err;
+				    console.log(search + ' saved to cachedData!');
+				});
+
+				fs.readFile('./cachedData/' + search + '.json', 'utf-8', (err, data) => {
+					if (err) throw err;
+					var apiData2 = JSON.parse(data);
+					res.status(200).render('moviePage', {apiData2});
+				});
+			}
+	}
 }
 
 app.listen(port, function () {
